085a31ac766f3482144f1c90f400e7799e656473
Alex Choi <alexander.e.choi@gmail.com>
2024-11-06 17:31:33 -0800

    combining constraints leads to unstable constraint convergence if the combination is positive semidefinite

--- src/pydrostat/constraint/fixed_vertex.py
+++ src/pydrostat/constraint/fixed_vertex.py
@@ -12,18 +12,35 @@ class FixedVertex(IConstraint):
         self.initial_positions = structure.positions[self.fixed_vertices]
 
     def calculate_constraints(self, structure: IStructure):
-        num_constraints = len(self.fixed_vertices)
+        # num_constraints = len(self.fixed_vertices)
+        # relative_vecs = (
+        #     structure.positions[self.fixed_vertices] - self.initial_positions
+        # )
+        # constraints = 0.5 * (relative_vecs * relative_vecs).sum(axis=1)
+
+        # jacobians = np.zeros((num_constraints,) + structure.positions.shape)
+        # djac_dts = np.zeros((num_constraints,) + structure.positions.shape)
+
+        # jacobians[np.arange(num_constraints), self.fixed_vertices, :] = relative_vecs
+        # djac_dts[np.arange(num_constraints), self.fixed_vertices, :] = (
+        #     structure.velocities[self.fixed_vertices]
+        # )
+
+        dim = structure.positions.shape[-1]
+        num_constraints = len(self.fixed_vertices) * dim
         relative_vecs = (
             structure.positions[self.fixed_vertices] - self.initial_positions
         )
-        constraints = 0.5 * (relative_vecs * relative_vecs).sum(axis=1)
+        constraints = relative_vecs.flatten()
+
+        dim_idx = np.arange(dim)
+        dim_IDX, fixed_IDX = np.meshgrid(dim_idx, self.fixed_vertices)
 
         jacobians = np.zeros((num_constraints,) + structure.positions.shape)
         djac_dts = np.zeros((num_constraints,) + structure.positions.shape)
 
-        jacobians[np.arange(num_constraints), self.fixed_vertices, :] = relative_vecs
-        djac_dts[np.arange(num_constraints), self.fixed_vertices, :] = (
-            structure.velocities[self.fixed_vertices]
-        )
+        jacobians[
+            np.arange(num_constraints), fixed_IDX.flatten(), dim_IDX.flatten()
+        ] = 1
 
         return constraints, jacobians, djac_dts
