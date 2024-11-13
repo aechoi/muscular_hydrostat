bc6cf5954c9201af93e08fdf55abb54179b78c36
Alex Choi <alexander.e.choi@gmail.com>
2024-11-13 13:36:52 -0800

    add vectorized constant volume constraint assuming common cell structure

--- src/pydrostat/constraint/constant_volume.py
+++ src/pydrostat/constraint/constant_volume.py
@@ -5,7 +5,8 @@ from ..structure.structure_interface import IStructure
 
 
 class ConstantVolume(IConstraint):
-    """Constant volume constraint for a 3D cell. Assume that"""
+    """Constant volume constraint for a 3D cell. Assume that the cells may have
+    different structures from each other."""
 
     def __init__(self):
         self.initial_volumes = None
@@ -77,49 +78,72 @@ class ConstantVolume(IConstraint):
             jacobians[c_idx] = jacobian
             djac_dts[c_idx] = djac_dt
 
-            # jacobian = np.zeros(structure.positions.size)
-            # djacdt = np.zeros(structure.positions.size)
-
-            # apex_position = structure.positions[cell.vertices[0]]
-            # apex_velocity = structure.velocities[cell.vertices[0]]
-
-            # relative_positions = (
-            #     structure.positions[cell.triangles] - apex_position
-            # )  # Tx3xD
-            # relative_velocities = structure.velocities[cell.triangles] - apex_velocity
-            # volumes = np.linalg.det(relative_positions)  # T
-            # volume = np.sum(volumes)
-
-            # position_inverse = np.linalg.inv(relative_positions)  # Tx3xD
-
-            # cofactors = volumes[:, None, None] * position_inverse.swapaxes(
-            #     -1, -2
-            # )  # TxDx3
-            # adjugate = cofactors.swapaxes(-1, -2)
-            # jacobian[get_vec_indices([0])] = -np.sum(cofactors, axis=(0, 1))
-            # vec_indices = get_vec_indices(cell.triangles.flatten())
-            # np.add.at(jacobian, vec_indices, cofactors.flatten())
-
-            # dcofactors = (
-            #     np.trace(adjugate @ relative_velocities, axis1=1, axis2=2)[
-            #         :, None, None
-            #     ]
-            #     * position_inverse
-            #     - adjugate @ relative_velocities @ position_inverse
-            # )
-            # djacdt[get_vec_indices([0])] = -np.sum(dcofactors, axis=(0, 1))
-            # np.add.at(djacdt, vec_indices, dcofactors.flatten())
-
-            # constraints[c_idx] = volume - self.initial_volumes[c_idx]
-            # jacobians[c_idx] = jacobian.reshape(-1, 3)
-            # djac_dts[c_idx] = djacdt.reshape(-1, 3)
-
         return constraints, jacobians, djac_dts
 
 
-# def get_vec_indices(vertex_indices: list):
-#     """Return the list of indices that correspond to the vertex indices."""
-#     if type(vertex_indices) is int:
-#         vertex_indices = [vertex_indices]
-#     vertex_indices = np.array(vertex_indices)
-#     return (vertex_indices[:, None] * 3 + np.arange(3)[None, :]).flatten()
+class ConstantVolumeCommon(IConstraint):
+    """Constant volume constraint for a 3D cell. Assume that the cells have a common
+    structure."""
+
+    def __init__(self):
+        self.initial_volumes = None
+
+    def initialize_constraint(self, structure: IStructure):
+        self.vertices = np.array([cell.vertices for cell in structure.cells])  # CxV
+        self.triangles = np.array([cell.triangles for cell in structure.cells])  # CxTxS
+
+        apex_positions = structure.positions[self.vertices[:, 0]]  # CxD
+        relative_positions = (
+            structure.positions[self.triangles] - apex_positions[:, None, None, :]
+        )  # CxTx3xD - CxD
+        tet_volumes = np.linalg.det(relative_positions)  # CxT
+        self.initial_volumes = np.sum(tet_volumes, axis=1)  # C
+
+    def calculate_constraints(self, structure: IStructure):
+        if self.initial_volumes is None:
+            raise ValueError(
+                "Initial volumes have not been calculated. Call initialize_constraint()"
+            )
+        pos = structure.positions
+        vel = structure.velocities
+
+        constraints = np.zeros(len(structure.cells))
+        jacobians = np.zeros((len(structure.cells),) + pos.shape)
+        djac_dts = np.zeros((len(structure.cells),) + pos.shape)
+
+        apex_positions = pos[self.vertices[:, 0]]
+        apex_velocities = vel[self.vertices[:, 0]]
+
+        relative_positions = pos[self.triangles] - apex_positions[:, None, None, :]
+        relative_velocities = (
+            vel[self.triangles] - apex_velocities[:, None, None, :]
+        )  # CxTxSxD
+
+        tet_volumes = np.linalg.det(relative_positions)
+        volumes = np.sum(tet_volumes, axis=1)
+        constraints = volumes - self.initial_volumes
+
+        position_inverse = np.linalg.inv(relative_positions)  # CxTxDxS
+        cofactors = tet_volumes[:, :, None, None] * position_inverse.swapaxes(
+            -1, -2
+        )  # CxTxSxD
+        adjugates = cofactors.swapaxes(-1, -2)  # CxTxDxS
+
+        jacobians[:, 0] = -np.sum(cofactors, axis=(1, 2))  # CxD
+
+        dcofactors = (
+            np.sum(cofactors * relative_velocities, axis=(-1, -2))[:, :, None, None]
+            * position_inverse
+            - adjugates @ relative_velocities @ position_inverse
+        )
+        djac_dts[:, 0] = -np.sum(dcofactors, axis=(1, 2))  # CxD
+
+        for cell_idx, _ in enumerate(structure.cells):
+            np.add.at(
+                jacobians[cell_idx], self.triangles[cell_idx], cofactors[cell_idx]
+            )
+            np.add.at(
+                djac_dts[cell_idx], self.triangles[cell_idx], dcofactors[cell_idx]
+            )
+
+        return constraints, jacobians, djac_dts
