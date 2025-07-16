fixed the reaction force calculation and got sensing and control working. Now can do optimization and finish jax implementation

src/pydrostat/actor.py
@@ -36,6 +36,7 @@ class Actor:
             []
         )  # When placed in an environment, these are added/removed
         self.observations = {}
+        self.environment = None
 
     def reset_state(self):
         """Reset the actor's state to the initial state."""
@@ -43,17 +44,19 @@ class Actor:
         self.control = jnp.zeros(self.model.num_controls)
 
     def set_environment(self, environment):
+        self.environment = environment
         self.current_obstacles = self.model.set_environment(
             environment, self.state, self.current_obstacles
         )
 
-    def sense(self, environment) -> dict[str, jnp.ndarray]:
+    def sense(self) -> dict[str, jnp.ndarray]:
         """Take sensor measurements for all sensors and return a dictionary of data.
 
         Returns:
             A dictionary of sensor data where each key is the sensor type."""
         for sensor in self.sensors:
-            self.observations.update(sensor.sense(self.state, environment))
+            sensor_output = sensor.sense(self.model, self.state, self.environment)
+            self.observations.update({sensor.sensor_type: sensor_output})
         return self.observations
 
     def estimate_state(self) -> jnp.ndarray:
@@ -79,6 +82,7 @@ class Actor:
 
         Returns:
             The next state of the model after applying the control policy."""
+        self.sense()
         self.control = self.calculate_control(t)
         next_state = self.model.discrete_dynamics(self.state, self.control, t, dt)
         self.state = next_state

src/pydrostat/control/hand_tuned_gradient.py
@@ -44,7 +44,9 @@ class HandTunedGradient(IPolicy):
             if forward_backward_gradient > 0:
                 actuator_index = np.array(
                     [
-                        np.where(np.all(structure.edges == sorted(edge), axis=1))[0]
+                        np.where(
+                            np.all(np.array(structure.edges) == sorted(edge), axis=1)
+                        )[0]
                         for edge in cell.edges[-4:]
                     ]
                 ).flatten()
@@ -52,7 +54,9 @@ class HandTunedGradient(IPolicy):
             else:
                 actuator_index = np.array(
                     [
-                        np.where(np.all(structure.edges == sorted(edge), axis=1))[0]
+                        np.where(
+                            np.all(np.array(structure.edges) == sorted(edge), axis=1)
+                        )[0]
                         for edge in cell.edges[4:-8]
                     ]
                 ).flatten()
@@ -65,7 +69,9 @@ class HandTunedGradient(IPolicy):
             activations = rel_vertices @ desired_motion
             actuator_index = np.array(
                 [
-                    np.where(np.all(structure.edges == sorted(edge), axis=1))[0]
+                    np.where(np.all(np.array(structure.edges) == sorted(edge), axis=1))[
+                        0
+                    ]
                     for edge in cell.edges[4:-8]
                 ]
             ).flatten()
@@ -73,7 +79,9 @@ class HandTunedGradient(IPolicy):
 
             actuator_index = np.array(
                 [
-                    np.where(np.all(structure.edges == sorted(edge), axis=1))[0]
+                    np.where(np.all(np.array(structure.edges) == sorted(edge), axis=1))[
+                        0
+                    ]
                     for edge in cell.edges[8:-4]
                 ]
             ).flatten()

src/pydrostat/model/constrained_dynamics.py
@@ -207,7 +207,10 @@ class ConstrainedDynamics(DynamicModel):
 
         pos, vel = self.state2posvel(state)
         constraints, jacobian, djacobian_dt = self._calculate_constraints(state)
-        front_matrix = jacobian @ (self.inv_masses[None, :] * jacobian).T
+        front_matrix = jacobian @ (self.inv_masses[:, None] * jacobian.T)
+        front_matrix = (
+            front_matrix + jnp.eye(front_matrix.shape[0]) * 1e-6
+        )  # Regularization
         dependent_array = -(
             djacobian_dt @ vel.ravel()
             + jacobian @ (self.inv_masses * explicit_forces.ravel())